0952cff39534ae1d53dbb00d163d93e8af944674
Neil Tucker <49943805+neiltucker@users.noreply.github.com>
2020-05-14 14:50:26 -0400

    Add files via upload

--- azuresql.py
+++ azuresql.py
@@ -3,8 +3,9 @@ server = 'azsrvazdb01.database.windows.net'
 database = 'database001'
 username = 'sqllogin1'
 password = 'Password1234'
-driver= '{ODBC Driver 17 for SQL Server}'
-cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='db1';UID='sqllogin1';PWD='Password1234)
+driver= '{SQL Server}'
+driver1= '{ODBC Driver 17 for SQL Server}'
+cnxn = pyodbc.connect('DRIVER=driver;SERVER=server;PORT=1433;DATABASE=database;UID=username;PWD=password')
 cursor = cnxn.cursor()
 cursor.execute("SELECT * From dbo.employees")
 row = cursor.fetchone()
